eeac790a7c52e2257473eb3f4aa20efe7a84235a
Benjamin Heisig <benjamin@heisig.name>
2017-04-18 20:25:12 +0200

    Move Piwik code to external JS file to set content security policy more strictly

---
+++ js/tracker.js
@@ -0,0 +1,8 @@
+var idSite = 2;
+var piwikTrackingApiUrl = '//benjamin.heisig.name/piwik/piwik.php';
+
+var _paq = _paq || [];
+_paq.push(['setTrackerUrl', piwikTrackingApiUrl]);
+_paq.push(['setSiteId', idSite]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
